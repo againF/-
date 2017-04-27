f8ca000ab7168f8206271306ecce260073ee86d4
againF <wyffcld@163.com>
2017-04-27 16:44:39 +0800

    Create 用对象查找.js

---
+++ 用对象查找.js
@@ -0,0 +1,16 @@
+# 用对象查找
+function phoneticLookup(val) {
+var result = "";
+var lookup = {
+  alpha : "Adams",
+  bravo : "Boston",
+  charlie : "Chicago",
+  delta : "Denver",
+  echo : "Easy",
+  foxtrot : "Frank"
+};
+result = lookup[val];
+return result;
+}
+// Change this value to test
+phoneticLookup("charlie");
